0fe8cff8db0b9a1d1546e64d66a20f383cd40fb2
Gene <generoso.martello@acsoftware.it>
2019-07-10 12:07:30 +0200

    added fixme note as a reminder for Plotly issue

--- projects/core/src/lib/hyperiot-base/hyperiot-base.module.ts
+++ projects/core/src/lib/hyperiot-base/hyperiot-base.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DataStreamService } from './services/data-stream.service';
-import { WidgetComponent } from './widgets/widget.component';
 import { WidgetChartComponent } from './widgets/widget-chart.component';
 
 declare const require;
@@ -29,6 +28,9 @@ export * from './widgets/widget-chart.component';
 export * from './widgets/data/data-packet-filter';
 export * from './widgets/data/time-series';
 
+// FIXME: This old-fashoned import/patch is due to the following issue
+// FIXME: https://github.com/plotly/plotly.js/issues/3518
+// FIXME: it should be fixed in Plotly >= 2.0.0
 var PlotlyJS = require('./plotly.patched');
 import { PlotlyModule } from 'angular-plotly.js';
 PlotlyModule.plotlyjs = PlotlyJS;
