2a10abac5612ec3f387e31ee12aae08053a1b269
Marco Brunet <marco.brunet@acsoftware.it>
2022-02-07 13:04:49 +0100

    Update 11

--- src/app/test-idb/test-idb.component.spec.ts
+++ src/app/test-idb/test-idb.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TestIdbComponent } from './test-idb.component';
 
@@ -6,7 +6,7 @@ describe('TestIdbComponent', () => {
   let component: TestIdbComponent;
   let fixture: ComponentFixture<TestIdbComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TestIdbComponent ]
     })
--- src/app/test/service-test.component.spec.ts
+++ src/app/test/service-test.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ServiceTestComponent } from './service-test.component';
 
@@ -6,7 +6,7 @@ describe('TestComponent', () => {
   let component: ServiceTestComponent;
   let fixture: ComponentFixture<ServiceTestComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ServiceTestComponent]
     })
