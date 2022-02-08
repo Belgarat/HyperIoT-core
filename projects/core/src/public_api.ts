b87910a0f45de1a302eb50bc8e163d0da62f2704
Alessio Serra <alessio.serra@acsoftware.it>
2022-02-08 15:58:27 +0100

    Updated library dependencies (alarms-client)

--- projects/core/src/public_api.ts
+++ projects/core/src/public_api.ts
@@ -14,6 +14,7 @@ export {
 export { DataChannel } from './lib/hyperiot-base/services/data-stream.service';
 
 export { AlgorithmsService } from './lib/hyperiot-client/algorithms-client/api-module/index';
+export { AlarmsService } from './lib/hyperiot-client/alarms-client/api-module/index';
 export { AreasService } from './lib/hyperiot-client/area-client/api-module/index';
 export { AssetscategoriesService } from './lib/hyperiot-client/asset-category-client/api-module/index';
 export { AssetstagsService } from './lib/hyperiot-client/asset-tag-client/api-module/index';
@@ -35,6 +36,7 @@ export { DashboardwidgetsService } from './lib/hyperiot-client/dashboard-widgets
 export { CompaniesService } from './lib/hyperiot-client/company-client/api-module/index';
 export { WidgetsService } from './lib/hyperiot-client/widgets-client/api-module/index';
 
+export { ApiModule as AlarmsClientModule } from './lib/hyperiot-client/alarms-client/api-module/api.module';
 export { ApiModule as AlgorithmsClientModule } from './lib/hyperiot-client/algorithms-client/api-module/api.module';
 export { ApiModule as AreasClientModule } from './lib/hyperiot-client/area-client/api-module/api.module';
 export { ApiModule as AssetscategoriesClientModule } from './lib/hyperiot-client/asset-category-client/api-module/api.module';
