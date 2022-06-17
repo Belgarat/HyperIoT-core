c6b2603461c0a42fdc25bda63e19ff70b7d26906
Stanislav Antokhi <stanislav.antokhi@acsoftware.it>
2022-06-17 10:17:51 +0200

    Added additional comments on the replacing code section

--- generator/acsgen.sh
+++ generator/acsgen.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#acsgenerator
 
 NG_VERSION=12.2.16
-
 configurationFile="./config"
 source $configurationFile
 
@@ -31,6 +29,7 @@ echo -e "Configuration:\nswaggerPath:\t'${swaggerPath}'\ndocFile:\t'${docFile}'\
 counterGen=0
 
 if [ -f ${modelFolder/*} ]; then rm -r ${modelFolder}/*; fi
+
 while read line; do
 	read -r -a splitted <<< "$line"
 	echo -e "${CYAN}Swagger: generating '${splitted[0]}'${NC}"
@@ -57,17 +56,24 @@ done
 
 echo -e "\n${GREEN}CODE GENERATION COMPLETED  (${counterGen}/${counterGen})${NC}\n"
 
-counterRep=0
 
+# NOTE: the following code is used to replace the model imports 
+# in each generated ng module from swagger with the alias path 'acs-models' defined in:
+# 1. tsconfig.json (for development purposes);
+# 2. tsconfig.lib.prod.json (required for the correct use of the library in the production environment)
+# 3. tsconfig.lib.json
+# 
+# Example: 
+# import { AssetTag } from '../../../models/assetTag'; ======> import { AssetTag } from 'acs-models';
+
+counterRep=0
 while read line2; do
 	counterRep=$((counterRep+1))
 	read -r -a repl <<< "$line2"
 	echo -e "${counterRep} sed: ${repl[0]}: '${repl[1]}' -> '${repl[2]}'"
 	sed -i "s@${repl[1]}@${repl[2]}@g" ${repl[0]} #WINDOWS
-	#sed -i '' "s@${repl[1]}@${repl[2]}@g" ${repl[0]} #MAC/LINUX
+	# sed -i '' "s@${repl[1]}@${repl[2]}@g" ${repl[0]} #MAC/LINUX
 	
 done <"${replaceFile}"
 
 echo -e "\n${GREEN}REPLACEMENT COMPLETED  (${counterRep}/${counterRep})${NC}\n"
-
-sleep 5
